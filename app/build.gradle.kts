56e118feaa82a675ea75ca61487e20d69e7cb214
geopaul94 <geopaul94@gmail.com>
2026-06-30 17:05:54 +0530

    Bump minSdk to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.loanmate"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
